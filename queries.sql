73660d69d43557884f138bf9c751923941163bbd
Alexandre Siedschlag <88800338+AlexandreSiedschlag@users.noreply.github.com>
2022-03-22 15:54:58 -0300

    Final Commit

--- queries.sql
+++ queries.sql
@@ -13,7 +13,49 @@ CREATE TABLE groups(
     group_names varchar(255),
     PRIMARY KEY(id_group)
     FOREIGN KEY (id)
-)
+);
+INSERT INTO tasks(  name,
+                    description,
+                    duration,
+                    priority)
+            Values(
+                    'abrir',
+                    'abrir com a mao direita',
+                    '1000',
+                    '1'
+            );
+INSERT INTO tasks(  name,
+                    description,
+                    duration,
+                    priority)
+            Values(
+                    'fechar',
+                    'fechar com a mao esquerda',
+                    '2000',
+                    '2'
+            );
+INSERT INTO tasks(  name,
+                    description,
+                    duration,
+                    priority)
+            Values(
+                    'olhar',
+                    'olhar com os dois olhos',
+                    '3000',
+                    '1'
+            );
+INSERT INTO tasks(  name,
+                    description,
+                    duration,
+                    priority)
+            Values(
+                    'comer',
+                    'comer com a boca',
+                    '3000',
+                    '1'
+            );
+
+
 SELECT * FROM tasks WHERE priority<3;
 SELECT * FROM tasks WHERE duration<=30;
 SELECT * FROM tasks WHERE duration>30;
