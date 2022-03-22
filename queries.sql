553ad404fdc580c1581cf5e77174e44346c7286d
Alexandre Siedschlag <88800338+AlexandreSiedschlag@users.noreply.github.com>
2022-03-22 15:14:04 -0300

    Second Commit

--- queries.sql
+++ queries.sql
@@ -5,7 +5,15 @@ CREATE TABLE tasks(
     description varchar(255),
     duration float,
     priority int,
+    PRIMARY KEY (id)
+    
 );
+CREATE TABLE groups(
+    id_group int,
+    group_names varchar(255),
+    PRIMARY KEY(id_group)
+    FOREIGN KEY (id)
+)
 SELECT * FROM tasks WHERE priority<3;
 SELECT * FROM tasks WHERE duration<=30;
 SELECT * FROM tasks WHERE duration>30;
